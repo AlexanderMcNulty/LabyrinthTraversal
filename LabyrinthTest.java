cf438075d4f8ff0df79719d128223e1251292075
Alexander McNulty <Alexander.mcnulty92@gmail.com>
2019-04-14 00:53:02 -0700

    test labyrinth generation

--- LabyrinthTest.java
+++ LabyrinthTest.java
@@ -2,8 +2,9 @@
 import javax.annotation.Generated;
 
 import org.junit.Test;
+import java.util.*;
+import org.junit.Assert;
 
-@Generated(value = "org.junit-tools-1.1.0")
 public class LabyrinthTest {
 
 	private Labyrinth createTestSubject() {
@@ -39,5 +40,33 @@ public class LabyrinthTest {
 		String fileName = "maze10.txt";
 		testSubject.readLabyrinth(fileName);		
 		testSubject.printLabyrinthGraph();
+		for(int i = 0; i < testSubject.traceBFSBestPath().size(); i++) {
+            System.out.println(testSubject.traceBFSBestPath().get(i));
+        }
 	}
+
+	@Test
+	public void testPrintMaze() throws Exception {
+		Labyrinth testSubject;
+		testSubject = createTestSubject();
+		String fileName = "maze10.txt";
+		testSubject.readLabyrinth(fileName);	
+		testSubject.traceBFSBestPath();
+		testSubject.printMaze();
+	}
+	
+	@Test
+	public void testGenerateMaze() throws Exception {
+		Labyrinth testSubject;
+		int length = 20;
+		// default test
+		System.out.println("start");
+		testSubject=createTestSubject();
+		testSubject.GenerateMaze(length);
+		testSubject.traceBFSBestPath();
+		testSubject.printMaze();
+		System.out.println("finish");
+	}
+	
+	
 }
